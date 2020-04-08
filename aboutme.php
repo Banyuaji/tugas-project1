ba95e2971a25a55c8bb0b444b7f59328594a4d02
Adhi Satria Banyuaji <53932334+Banyuaji@users.noreply.github.com>
2020-04-08 16:15:34 +0700

    menghapus alamat

--- aboutme.php
+++ aboutme.php
@@ -2,7 +2,6 @@
     echo "Nama : Adhi satria banyuaji \n";
     echo "Kelas : XI RPL 2 \n";
     echo "Asal sekolah : SMK Taruna Bhakti \n";
-    echo "Alamat : Depok, Jl. Damar Laut 2 No.127 \n"
     echo "No Hp : 089655246312 \n";
     echo "E-mail : thethex24@gmail.com \n";
 ?>
