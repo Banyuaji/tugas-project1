129b90dd0812cc9ad79a1347bb61417f52925bcc
Adhi Satria Banyuaji <53932334+Banyuaji@users.noreply.github.com>
2020-04-08 16:08:13 +0700

    Add files via upload

---
+++ aboutme.php
@@ -0,0 +1,6 @@
+<?php
+    echo "Nama : Adhi satria banyuaji \n";
+    echo "Kelas : XI RPL 2 \n";
+    echo "Asal sekolah : SMK Taruna Bhakti \n";
+    echo "Alamat : Depok, Jl. Damar Laut 2 No.127 \n"
+?>
