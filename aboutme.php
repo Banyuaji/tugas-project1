ae75726d90b89621b9d401b5a8b86378e81c2e8d
Adhi Satria Banyuaji <53932334+Banyuaji@users.noreply.github.com>
2020-04-08 16:15:03 +0700

    menambahkan hp dan email

--- aboutme.php
+++ aboutme.php
@@ -3,4 +3,6 @@
     echo "Kelas : XI RPL 2 \n";
     echo "Asal sekolah : SMK Taruna Bhakti \n";
     echo "Alamat : Depok, Jl. Damar Laut 2 No.127 \n"
+    echo "No Hp : 089655246312 \n";
+    echo "E-mail : thethex24@gmail.com \n";
 ?>
